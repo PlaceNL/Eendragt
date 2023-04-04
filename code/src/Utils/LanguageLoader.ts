5e058850f93a21eb962ccb57a208fbc5f034a69d
theelicht <melleeluc@gmail.com>
2023-04-04 16:27:57 +0200

    Add default values to LangConfig to deal with missing values

--- code/src/Utils/LanguageLoader.ts
+++ code/src/Utils/LanguageLoader.ts
@@ -1,12 +1,19 @@
 import * as fs from 'fs';
 
+type LangConfig = {
+    ROLE_SELECT_PLACEHOLDER: string,
+    I_WANT_TO_HELP: string
+};
+
 // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
 class LanguageLoader {
-    public static LangConfig = {
-        ROLE_SELECT_PLACEHOLDER: '',
+    public static LangConfig: LangConfig = {
+        ROLE_SELECT_PLACEHOLDER: 'Selecteer een rol',
+        I_WANT_TO_HELP: '🌷 Ik wil meehelpen!'
     };
     public static LoadLanguageConfig(languageCode: string) {
         const json = fs.readFileSync(`lang/${languageCode}`, 'utf8');
-        this.LangConfig = JSON.parse(json);
+        const parsedObject: Partial<LangConfig> = JSON.parse(json);
+        this.LangConfig = Object.assign(this.LangConfig, parsedObject);
     }
 }
