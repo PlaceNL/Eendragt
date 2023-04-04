c3e9d5583b97f3deb12cbdd976e758b86e03c7a8
Luc Melleé <melleeluc@gmail.com>
2023-04-04 21:59:10 +0200

    Add ApplicationHandler's simplest strings to lang file

--- code/src/Utils/LanguageLoader.ts
+++ code/src/Utils/LanguageLoader.ts
@@ -1,19 +1,23 @@
 import * as fs from 'fs';
 
 type LangConfig = {
-    ROLE_SELECT_PLACEHOLDER: string,
-    I_WANT_TO_HELP: string
+    ROLE_SELECTOR_PLACEHOLDER: string | null,
+    I_WANT_TO_HELP: string | null
 };
 
 // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-class LanguageLoader {
+export default class LanguageLoader {
     public static LangConfig: LangConfig = {
-        ROLE_SELECT_PLACEHOLDER: 'Selecteer een rol',
+        ROLE_SELECTOR_PLACEHOLDER: 'Selecteer een rol',
         I_WANT_TO_HELP: '🌷 Ik wil meehelpen!'
     };
     public static LoadLanguageConfig(languageCode: string) {
-        const json = fs.readFileSync(`lang/${languageCode}`, 'utf8');
-        const parsedObject: Partial<LangConfig> = JSON.parse(json);
-        this.LangConfig = Object.assign(this.LangConfig, parsedObject);
+        const json = fs.readFileSync(`lang/${languageCode}.json`, 'utf8');
+        try {
+            const parsedObject: Partial<LangConfig> = JSON.parse(json);
+            this.LangConfig = Object.assign(this.LangConfig, parsedObject);
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
